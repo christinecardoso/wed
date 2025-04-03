6a1642e2bf296bc2fe8b0bbbb74c37b39a244f0e
christinecardoso <christinesalafia@gmail.com>
2025-04-03 12:13:23 -0400

    Update _config.ts
    cache busting

--- _config.ts
+++ _config.ts
@@ -1,8 +1,12 @@
 import lume from "lume/mod.ts";
 import plugins from "./plugins.ts";
+import cacheBusting from "lume/middlewares/cache_busting.ts";
 
 const site = lume({
   src: "./src",
+  server: {
+    middlewares: [cacheBusting()],
+  },
 });
 
 site.use(plugins());
